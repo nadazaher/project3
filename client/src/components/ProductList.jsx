32ca6542450c7528b4116da376bf3bd7fb0e1641
David Whitlatch <davidtwhitlatch@gmail.com>
2018-08-21 10:17:08 -0400

    added commenting to make the function more clear

--- client/src/components/ProductList.jsx
+++ client/src/components/ProductList.jsx
@@ -21,9 +21,13 @@ class ProductList extends Component {
 
   render() {
     return (
+      // filtering through the array of products either by company of user input depending on the provided function
+      // then mapping through the results to display either the data or an inline edit form
       this.props.products.filter(this.props.filterFN).map((product) =>
+      // checking if productIsEditing variable from state is truthy or falsey
         this.state.productIsEditing === product.id
           ?
+          // displaying an inline editing form
           (<div className="columns" key={product.id}>
             <div className="column is-one-fourth" ><input name="productURL" value={this.state.productURL} type="text" onChange={this.handleChange} /></div>
             <div className="column is-one-fourth" ><input name="productName" value={this.state.productName} type="text" onChange={this.handleChange} /></div>
@@ -32,6 +36,7 @@ class ProductList extends Component {
               e.preventDefault();
               // const { productName } = this.state;
               // this.props.updateProduct({ ...product, productName });
+              // reseting the state back to null/empty from "edit" button event listener
               this.setState({
                 productURL: '',
                 productName: '',
@@ -42,6 +47,7 @@ class ProductList extends Component {
             }>Submit</button></div>
           </div>)
           :
+          // displaying basic product data with edit button
           (<div className="columns" key={product.id} >
             <div className="column is-one-fifth"><figure className="image is-96x96">
               <img src={product.logo} />
@@ -50,6 +56,7 @@ class ProductList extends Component {
             <div className="column is-one-fifth">{product.msrp}</div>
             <div className="column is-one-fifth"><button value={product.id} onClick={((e) => {
               e.preventDefault();
+              // setting specific product data for edit form
               this.setState({
                 productIsEditing: product.id,
                 productURL: product.logo,
