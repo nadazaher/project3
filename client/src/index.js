8f651a6bd68179a150fb095152d39fb59e7aa02d
NADA ZAHER <zahernada94@gmail.com>
2018-08-22 18:29:52 -0400

    font-awesome icons work

--- client/src/index.js
+++ client/src/index.js
@@ -4,10 +4,10 @@ import './index.css';
 import App from './App';
 import 'bulma/css/bulma.css';
 
-// import { library } from '@fortawesome/fontawesome-svg-core';
-// import { fas } from '@fortawesome/fontawesome-svg-core';
-// import { far } from '@fortawesome/fontawesome-svg-core';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
 
-// library.add(fas,far);
+library.add(fas,far);
 
 ReactDOM.render(<App />, document.getElementById('root'));
