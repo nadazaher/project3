afa8f2d5fb508947086921798546d91e1d451f3f
David Whitlatch <davidtwhitlatch@gmail.com>
2018-08-22 21:14:06 -0400

    setup backend express to use Favorites

--- app/models/favorites.js
+++ app/models/favorites.js
@@ -1,35 +1,27 @@
-// const { db } = require('../config/connection');
+const { db } = require('../config/connection');
 
-// module.exports = {
+module.exports = {
 
+  findAllFavorites() {
+    return db.many(`
+      SELECT * FROM favorites;
+    `);
+  },
 
-// createFavorite(favorite) {
-//     return db.one(`
-//     INSERT INTO favorites
-//     (user_id, product_id)
-//     VALUES
-//     ($/user_id/, $/product_id/)
-//     RETURNING *
-//     `, favorite);
-//   },
+  createFavorite(favorite) {
+    return db.one(`
+    INSERT INTO favorites
+    (user_id, product_id)
+    VALUES
+    ($/user_id/, $/product_id/)
+    RETURNING *
+    `, favorite);
+  },
 
-// deleteFavorite(id) {
-//     return db.none(`
-//     DELETE FROM favorites
-//     where id = $1`, id)
+  delete(id) {
+    return db.none(`
+    DELETE FROM favorites
+    where id = $1`, id);
+  },
 
-// },
-
-// findFavoritesByUserName() {
-//     return db.many(`
-//         SELECT
-//         u. id AS id, 
-//         f. user_id,
-//         f. product_id,
-//         u. username
-//         FROM favorites f 
-//         JOIN users u ON (u.id = f.user_id)
-//     `);
-// }
-// };
-
+};
