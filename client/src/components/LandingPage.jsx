cf5e61dbe2970f878bb63785bf01054a7595b025
David Whitlatch <davidtwhitlatch@gmail.com>
2018-08-22 20:18:04 -0400

    changed some stylings

--- client/src/components/LandingPage.jsx
+++ client/src/components/LandingPage.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 function LandingPage(props) {
   return (
-    <div >
-      This will be our landing page
+    <div className="container">
+      It may be obvious that Twix and M&Ms are both owned by Mars, but did you know that Breyers and Vaseline share a parent company Unilever? The majority of the brands you use daily are made by just 10 multinational conglomerates. By using our website, you can find out which companies make your favorite products and more details about each company. If you don’t see a product listed, you are encouraged to add it to its corresponding parent company. Let’s start by browsing through the list of companies or searching for a specific product!
         </div>
   );
 }
