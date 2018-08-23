70271c34e7e0923aab178aecebf6e230c580b2fd
James Gary <gmarchesi888@gmail.com>
2018-08-23 14:20:56 -0400

    Fixed landing page type.

--- client/src/components/LandingPage.jsx
+++ client/src/components/LandingPage.jsx
@@ -49,17 +49,16 @@ function LandingPage(props) {
 
           <section class="articles">
             <div class="column is-8 is-offset-2">
-
               <div class="card article">
                 <div class="card-content">
                   <div class="media">
                     <div class="media-content has-text-centered">
-                      <p class="title article-title">Who Makes My Product?</p>
+                      <h1 class="title is-1 has-text-dark">Who Makes My Product?</h1>
                     </div>
                   </div>
-                  <div class="subtitle is-size-4">
-                    <p>It may be obvious that Twix and M&Ms are both owned by Mars, but did you know that Breyers and Vaseline share a parent company, Unilever? The majority of the brands you use daily are made by just 10 multinational conglomerates. By using our website, you can find out which companies make your favorite products and more details about each company. If you don’t see a product listed, you are encouraged to add it to its corresponding parent company. Let’s start by browsing through the list of companies or searching for a specific product! </p>
-                  </div>
+
+                  <p class="subtitle is-4 has-text-black">It may be obvious that Twix and M&Ms are both owned by Mars, but did you know that Breyers and Vaseline share a parent company, Unilever? The majority of the brands you use daily are made by just 10 multinational conglomerates. By using our website, you can find out which companies make your favorite products and more details about each company. If you don’t see a product listed, you are encouraged to add it to its corresponding parent company. Let’s start by browsing through the list of companies or searching for a specific product! </p>
+
                 </div>
               </div>
 
