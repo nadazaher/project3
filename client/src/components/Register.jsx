967f18d7c790ad60889f75f2452f73714ea809cf
David Whitlatch <davidtwhitlatch@gmail.com>
2018-08-23 14:04:21 -0400

    styled Login and register forms

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -4,18 +4,18 @@ class Register extends Component {
     constructor(props) {
         super(props);
 
-    this.state = {
-        username: '',
-        password: ''
-    };
+        this.state = {
+            username: '',
+            password: ''
+        };
 
-    this.handleChange = this.handleChange.bind(this);
+        this.handleChange = this.handleChange.bind(this);
     }
 
-//use same comments for Login.jsx
+    //use same comments for Login.jsx
 
 
-// deconstruct [name] to = username and password out of target - so name and value taken from input in form
+    // deconstruct [name] to = username and password out of target - so name and value taken from input in form
     handleChange(ev) {
         const { name, value } = ev.target;
         this.setState({
@@ -26,34 +26,32 @@ class Register extends Component {
     render() {
         return (
             <div>
-                <h2> New User Registration </h2>
-                <form onSubmit={(e) => {
-                    e.preventDefault();
-                    // using this.props of this function because we gave it props in app.jsx switch function 
-                    this.props.handleRegisterSubmit(
+                <div class="login">
 
-                        // using this.state because we set state above
-                        this.state.username,
-                        this.state.password
-                    );
-                    // rerenders back to landing pag
-                    this.props.handleLinks('landing page');
-                }
-                }>
-                    <input
-                        type="text"
-                        name="username"
-                        placeholder="Username"
-                        value={this.state.username}
-                        onChange={this.handleChange} />
-                    <input
-                        type="password"
-                        name="password"
-                        placeholder="Password"
-                        value={this.state.password}
-                        onChange={this.handleChange} />
-                    <input type="submit" value="Submit" />
-                </form>
+                    <h2 class="login-header"> New User Registration </h2>
+
+                    <form class="login-container" onSubmit={(e) => {
+                        e.preventDefault();
+                        this.props.handleLoginSubmit(
+                            this.state.username,
+                            this.state.password
+                        );
+                        this.props.handleLinks('landing page');
+                    }
+                    } >
+                        <p><input type="text"
+                            name="username"
+                            placeholder="Username"
+                            value={this.state.username}
+                            onChange={this.handleChange} /></p>
+                        <p><input type="password"
+                            name="password"
+                            placeholder="Password"
+                            value={this.state.password}
+                            onChange={this.handleChange} /></p>
+                        <p><input type="submit" value="Register" /></p>
+                    </form>
+                </div>
             </div>
         )
     };
