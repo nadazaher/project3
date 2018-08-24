09818df60692c360cbe6db729038841b0891e2b7
David Whitlatch <davidtwhitlatch@gmail.com>
2018-08-24 01:41:32 -0400

    WIP setting up favorites stats

--- app/models/favorites.js
+++ app/models/favorites.js
@@ -39,8 +39,9 @@ module.exports = {
 
   countCompanies(id) {
     return db.one(`
-    SELECT COUNT(*), p.company_id AS company_id FROM favorites f
+    SELECT COUNT(*), p.company_id AS company_id, c.name AS name FROM favorites f
     JOIN products p ON f.product_id = p.id
+    JOIN companies c ON p.company_id = c.id
     WHERE p.company_id = $1`, id);
   },
 
