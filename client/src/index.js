379086997022301f9b63d8a2a76d6a3b741a92bf
NADA ZAHER <zahernada94@gmail.com>
2018-08-24 04:22:26 -0400

    added favicon

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import 'bulma/css/bulma.css';
+import Favicon from 'react-favicon';
 
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fas } from '@fortawesome/free-solid-svg-icons';
@@ -12,4 +13,8 @@ import { faStar } from '@fortawesome/free-regular-svg-icons'
 library.add(faStar);
 library.add(fas,far);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <div>
+<Favicon url= "http://downloadicons.net/sites/default/files/number-10-of--yellow-icon-50704.png" />
+<App />
+</div>, document.getElementById('root'));
